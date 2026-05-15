52d6688b5b92bd0e0e3cdb50c3d78efbda59d0a3
MAZZOLENI.22138 <mazzoleni.federico.studente@itispaleocapa.it>
2026-05-15 11:52:48 +0200

    Progetto di prova

--- Buffer.java
+++ Buffer.java
@@ -1,2 +1,35 @@
+import java.util.concurrent.Semaphore;
+
 public class Buffer {
+
+    private int value;
+
+    private Semaphore empty = new Semaphore(1);
+    private Semaphore full = new Semaphore(0);
+
+    public void produce(int number) {
+
+        try {
+            empty.acquire();
+
+            value = number;
+
+            System.out.println("Prodotto: " + value);
+
+            full.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void consume() {
+
+        try {
+            full.acquire();
+            System.out.println("Consumato: " + value);
+            empty.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Producer.java
+++ Producer.java
@@ -1,2 +1,21 @@
-public class Producer extends Thread{
+public class Producer extends Thread {
+
+    private Buffer buffer;
+
+    public Producer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            buffer.produce(i);
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
